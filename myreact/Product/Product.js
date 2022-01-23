72ac1925b5bb04e451d9ff24d807a286cab51c9b
samar <samarsekhar25@gmail.com>
2022-01-23 16:40:22 +0530

    okie

---
+++ myreact/Product/Product.js
@@ -0,0 +1,10 @@
+import React from "react";
+class Product extends React.Component {
+    render() {
+        console.log("Executing");
+        return <div>
+            <h4>Product Component</h4>
+        </div>
+    }
+}
+export default Product

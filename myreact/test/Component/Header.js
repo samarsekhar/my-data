c2d4b6b50f26581b601a2d24081a14213c0e6a34
samar <samarsekhar25@gmail.com>
2022-01-24 19:50:32 +0530

    okie

---
+++ myreact/test/Component/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+class Header extends React.Component {
+    render() {
+        return <div>
+            <h2>Header Component</h2>
+        </div>
+
+    }
+}
+export default Header

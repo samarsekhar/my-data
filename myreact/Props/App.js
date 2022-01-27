e2f01cc234f74e02f1a1bd9b9e967f54e41d289b
samar <samarsekhar25@gmail.com>
2022-01-27 17:44:44 +0530

    commit

---
+++ myreact/Props/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CompA from "./Props/CompA"
+
+let App = () => {
+    return (
+        <div>
+            <CompA />
+        </div>
+    )
+}
+export default App;

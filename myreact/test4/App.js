7d44144085d4ba900678ae961b9033cd96e56ab0
samar <samarsekhar25@gmail.com>
2022-01-28 18:29:09 +0530

    commit

---
+++ myreact/test4/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Product2 from "../myreact/test4/ProductEx/Product2";
+
+function App() {
+    return (
+        <>
+            <Product2 />
+        </>
+    )
+}
+export default App;

e3c7f0f6001c9f4786e9f7598c7c0422382ba0f5
samar <samarsekhar25@gmail.com>
2022-02-01 21:53:46 +0530

    commit 01.02

--- myreact/Axios_Ex/Axios.Ex1/Navbar/Navbar.jsx
+++ myreact/Axios_Ex/Axios.Ex1/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return <>
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="home" > React Routing </Link>
+                <Link to="Home" > React Routing </Link>
                 <div className="collapase navbar-collapase">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
